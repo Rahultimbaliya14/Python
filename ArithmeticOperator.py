3c9eb98ea85f47a4405a9d2c51933fa82aab052d
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-07-14 13:09:41 +0530

    Add files via upload

---
+++ ArithmeticOperator.py
@@ -0,0 +1,34 @@
+#Arithmetic Operator (+ , - , * , / , % , ** , //)
+
+#Asign Value To A And B
+A=10
+B=20
+
+"""(+)Addition Operator Calculate Sum Of A And B And Asign That's Value To C"""
+C=A+B
+print("Additon =",C)
+
+"""(-)Subtraction Operator Subtract Value Of B Into The A And Asign That's Value To C"""
+C=A-B
+print("Subtraction =",C)
+
+"""(*)Multiplication Operator Multply A And B And Asign That's Value To C"""
+C=A*B
+print("Multiplication =",C)
+
+"""(/)Division Operator Devide A Into B And Asign That's Value To C"""
+C=A/B
+print("Division =",C)
+
+"""(%)Modulus Operator Used To Find Remainder When A Is Devide By B And Asign That's Value To C"""
+C=A%B
+print("Modulus =",C)
+
+"""(**)Exponentiation Operator Used To Raise The A To Power B And Asign That's Value To C"""
+C=A**B
+print("Exponentiation =",C)
+
+
+"""(//)Floor Division Operator It Is Used To Find The Floor Result when A  is Devide By B"""
+C=A//B
+print("Floor Division =",C)
