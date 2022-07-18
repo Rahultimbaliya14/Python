c69e3b8242081a86e08e046c81741b38cd3deb66
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-07-18 08:16:01 +0530

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,11 @@
+#Input Is Used To Read The Input From User
+print("\n*********Find Number Is Odd Or Even*********\n")
+
+#This Statement Read The Value From User
+A=int(input("Enter The First Number:  "))
+
+if(A%2 == 0):
+    print("Number Is Even")
+else:
+    print("Number Is Odd")
+
