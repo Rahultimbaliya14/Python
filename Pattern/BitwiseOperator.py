7c1c06cc46c916fca8e331457a70772fa8b1a8de
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-07-28 17:36:59 +0530

    Add files via upload

---
+++ Pattern/BitwiseOperator.py
@@ -0,0 +1,28 @@
+#Asign Value To R And S
+R=3
+S=2
+
+"""(&)Bitwise And Operator Check The R And S Bits If Both Bit 1 Return The 1 Else 0"""
+T=R&S
+print(T)
+
+"""(|)Bitwise Or Operator Check The R And S Bits If Both Bit 0 Return The 0 Else 1"""
+T-R|S
+print(T)
+
+"""(^)Bitwise Xor Operator Check The R And S Bits If Both Bit Diffrent Return The 1 Else 0"""
+T=R^S
+print(T)
+
+"""(~)Bitwise Not Operator Returns One's complement Of The Bits"""
+T=~R
+print(T)
+
+"""(<<)BitWise Left Sift Operator Shift The Bit In Left Side Add 0"""
+T=S<<2
+print(T)
+
+"""(>>)BitWise Right Sift Operator Shift The Bit In Right Side Add 0"""
+T=R>>1
+print(T)
+
