d4c1b04365bbd2a46812d3959940026e7b1fef38
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-07-28 17:48:58 +0530

    Add files via upload
    The First Pattern Program Is Uploaded

---
+++ Pattern/Pattern1.py
@@ -0,0 +1,9 @@
+#Pattern 1
+print("THis is Statments",end=" ")
+row=int(input("Enter The Number Of Rows"))
+for i in range(row):
+    for space in range(row-i):
+          print(" ",end="")
+    for a in range(i+1):
+      print("* ",end="")
+    print()
