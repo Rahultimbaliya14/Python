efbd786fb3aa261c9c9b81be029d0cf5230ff07f
Rahul <rahultimbaliya555@gmail.com>
2022-09-28 09:05:05 +0530

    audio

--- Pattern/record.py
+++ Pattern/record.py
@@ -3,6 +3,10 @@ import pyttsx3
 import speech_recognition as sr 
 import time as t
 import pywhatkit as play
+i=0
+# while i<=10:
+#      play.sendwhatmsg_instantly("+916353647592","helo",tab_close=True)
+#      i+=1
 def main():
     r=sr.Recognizer()
     with sr.Microphone() as source:
@@ -48,10 +52,13 @@ def main():
                 engine.runAndWait()
                 print(b)
                 play.playonyt(b)
-                play.playon()
-
+            else:
+                engine.say("This Can Find On The Web")
+                engine.runAndWait()
+                play.search(b)
         except Exception as e:
             print(e)
 
+  
 if __name__=="__main__":
     main()
